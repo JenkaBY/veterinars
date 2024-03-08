f7fca2f3426a2b07b237a22e8844acca6c69c7a2
y.kuzmich <evgen.kuzmich@gmail.com>
2024-03-08 16:33:12 +0200

    unicode feature testing

--- route-service/src/main/java/io/by/lt/pl/Main.java
+++ route-service/src/main/java/io/by/lt/pl/Main.java
@@ -1,7 +1,10 @@
 package io.by.lt.pl;
 
 public class Main {
+   static String welcomeMsg = "Hey Java Developers! '🙋‍♂️";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world! U+1F4A9");
+        System.out.println(welcomeMsg);
     }
 }
